9b225de27e2fb5a8e7d23fb5ce856dd7a4b21f61
Debargha Basu <debargha.basu@in.abb.com>
2024-07-28 12:59:53 +0530

    Updated filter_pdf.py

--- filter_pdf.py
+++ filter_pdf.py
@@ -1,4 +1,6 @@
 import PyPDF2
+import functools
+import logging
 
 
 def filter_lines_with_words(pdf_path, words):
@@ -28,3 +30,34 @@ result_lines = filter_lines_with_words(pdf_path, words_to_search)
 
 for line in result_lines:
     print(line)
+
+# Adding another function
+
+
+def log_function(level=logging.INFO):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            logging.log(
+                level,
+                f"Calling function {func.__name__} with args: {args}, kwargs: {kwargs}",
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@log_function(level=logging.DEBUG)
+def add(a, b):
+    return a + b
+
+
+@log_function(level=logging.WARNING)
+def subtract(a, b):
+    return a - b
+
+
+print(add(3, 5))
+print(subtract(10, 3))
